6df95dd5c5698c82959e7174cff302e6a82bf435
Gaston82 <cabezaartaza@hotmail.com>
2021-11-25 21:12:08 +0100

    add a new service getBeerById

---
+++ src/services/getBeerById.js
@@ -0,0 +1,9 @@
+export async function getBeerById(id) {
+  console.log(id);
+  let url = `https://api.punkapi.com/v2/beers/${id}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
