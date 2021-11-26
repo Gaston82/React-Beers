3ec5ca51333f21a823d64ea3e4a4a56bb31d1ed8
Gaston82 <cabezaartaza@hotmail.com>
2021-11-26 23:48:09 +0100

    add css style to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,18 @@
-import '../styles/header.css'
+import "../styles/header.css";
 import logo from "../assets/images/logo192.png";
-export default function Header(){
-    return(
+export default function Header() {
+  return (
     <header className="header">
-     <h1>React Beers</h1>
-      <img className="header__logo" src={logo} alt="" />
+      <div className="container">
+        <div className="box-logo">
+          <h1 className="header__title">React Beers</h1>
+          <img className="header__logo" src={logo} alt="" />
+        </div>
+        <form action="">
+          <label htmlFor="">Search</label>
+          <input type="text" />
+        </form>
+      </div>
     </header>
-    )
+  );
 }
